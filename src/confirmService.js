a2562494399b3cdd95165720895fa332348e754a
Shohel Rana <cse_shohel@yahoo.com>
2019-10-15 10:47:10 +0600

    preparing confirm service

---
+++ src/confirmService.js
@@ -0,0 +1,25 @@
+class ConfirmService {
+    initialOptions = {
+        component: '.../confirm',
+        appendElement: 'body',
+        modalOptions: {
+            backdrop: 'static',
+            keyboard: false
+        },
+        title: 'Confirmation',
+        message: 'Are you sure?',
+        draggable: false,
+        data: {}
+    };
+
+    constructor() {
+        this.options = {...this.initialOptions};
+    }
+
+    show = (options) => {
+        options = Object.assign(this.options, options);
+
+    }
+}
+
+export default ConfirmService;
